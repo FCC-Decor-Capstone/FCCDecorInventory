a8734791ab63bf7cb983ff0943cc430c406b78f2
rmdaw00 <rmdaw00@gmail.com>
2021-03-22 21:21:59 -0400

    Merged Supplier

---
+++ WebContent/supplier/details.jsp
@@ -0,0 +1,69 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/functions" prefix = "fn" %>
+
+<%@ page isELIgnored="false" %>
+<%@ page import="models.Supplier,helpers.Constants" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Supplier ${requestScope.action} Details</title>
+	<link rel="stylesheet" href="/${Constants.URL_PREFIX}style.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	<script src="/${Constants.URL_PREFIX}scripts.js"></script>
+</head>
+<body>
+	<%@ include file="/_shared/message.jsp"%>
+	<h1>${requestScope.model.name} Supplier Details</h1>
+	<form class="toolBox"> 			
+		<input type="hidden" name="id" value="${requestScope.model.id}" />
+				<div>
+					<button type="submit" formaction="Edit" formmethod="get"><i class="fas fa-pen tablebtn" style="color:green;"> Edit</i></button>
+					<button type="button"  onClick="javascript:confirmGo('Are you sure you want to delete?','./Delete?id=${requestScope.model.id}')"><i class="fa fa-close tablebtn" style="color:red;"> Delete</i></button>
+				</div>
+				
+				<button type="submit" formaction="./." formmethod="get"><i class="fas fa-arrow-left"> Back to List</i></button>
+	</form>
+	<main class="rmdT">
+			<p>Supplier Name: ${requestScope.model.name}</p>
+			<c:if test="${not empty requestScope.model.contact}">
+				<p>Contact Name: ${requestScope.model.contact}</p>
+			</c:if>
+			<c:if test="${not empty requestScope.model.telephone}">
+				<p>Contact Telephone: ${requestScope.model.telephone}</p>
+			</c:if>
+			<c:if test="${not empty requestScope.model.comments}">
+				<p>Comments: ${fn:trim(requestScope.model.comments)}</p>
+			</c:if>
+		<section class="rmdT">
+
+				<table class="rmdT">				
+						<tr>
+							<th><span>Item</span></th>
+							<th><span>Category</span></th>
+							<th><span>Description</span></th>
+						</tr>
+			<c:choose>
+			<c:when test="${not empty requestScope.list}">
+						<c:forEach var="item" items="${requestScope.list}">		
+						<tr>											
+							<td><a href="./Details?id=${item.name}"><span>${item.name}</span></a></td>
+							<td><a href="./Details?id=${item.category}"><span>${item.category}</span></a></td>
+							<td><a href="./Details?id=${item.description}"><span>${item.description}</span></a></td>
+						</tr>
+					</c:forEach>
+				
+			</c:when>
+			<c:otherwise>
+				<tr><td colspan="3"><div>No Items Linked.</div></td></tr>
+			</c:otherwise>
+		</c:choose>
+			</table>
+		</section>
+	</main>
+</body>
+</html>
---
+++ src/controllers/EditSupplier.java
@@ -0,0 +1,93 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Supplier;
+
+/**
+ * Servlet implementation class EditSupplier
+ */
+@WebServlet(value="/Supplier/Edit")
+public class EditSupplier extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditSupplier() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatcher = request.getRequestDispatcher("");
+		if (request.getParameter("id") != null) {
+			try {
+				int id = Integer.parseInt(request.getParameter("id"));
+				Supplier model = Supplier.getByID(id);
+				if (model != null) {
+					request.setAttribute("model", Supplier.getByID(id));
+					request.setAttribute("action", "Edit");
+					dispatcher = request.getRequestDispatcher("/supplier/form.jsp");
+				} else {
+					request.setAttribute("ErrCtlMsg", "Supplier Not Found");
+				}
+			} catch (NumberFormatException nfe) {
+				request.setAttribute("ErrCtlMsg", "Can't fulfil request without ID");
+			} 
+		}
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/supplier/form.jsp");
+		Supplier newSupplier = new Supplier();
+		
+		
+		//Request Verification (within Model for clean code)
+			try {
+				request.setAttribute("errName", newSupplier.setId(Integer.parseInt(request.getParameter("id"))));
+			} catch (NumberFormatException nfe) {
+				response.getWriter().append("Invalid ID, please restart Edit form.");
+				return;
+			} 
+			request.setAttribute("errName", newSupplier.setName(request.getParameter("name")));
+			request.setAttribute("errContact", newSupplier.setContact(request.getParameter("contact")));
+			request.setAttribute("errTelephone", newSupplier.setTelephone(request.getParameter("telephone")));
+			request.setAttribute("errComments", newSupplier.setComments(request.getParameter("comments")));
+		
+			
+		request.setAttribute("model",newSupplier);
+			
+		if (newSupplier.hasError()) {
+			request.setAttribute("action", "Edit");
+			request.setAttribute("ErrCtlMsg", "Supplier Editing Error");
+		} else {
+			
+			Supplier.editByID(newSupplier);
+			request.setAttribute("SucCtlMsg", "Supplier Edited Successfully");
+			System.out.println("Edited" + newSupplier.getName() + " With ID of " + newSupplier.getId());
+			request.setAttribute("list", Supplier.getAll());
+			dispatcher = request.getRequestDispatcher("/supplier/table.jsp");
+			
+		}
+		
+		dispatcher.forward(request, response);
+	}
+
+}
